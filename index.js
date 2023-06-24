0d5bce08f39b132d34e12c2c5dcf36fe545b5da2
Samarth <samarthbsacharya@gmail.com>
2023-06-24 21:13:24 +0530

    working

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port =process.env.PORT
 const app =express();
 
 app.use(express.json());
-
+app.use(cors());
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', '*');
 //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
