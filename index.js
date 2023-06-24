5153f2b6ee927665a3eda6da781edd2b7815f80f
Samarth <samarthbsacharya@gmail.com>
2023-06-24 20:51:18 +0530

    working

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const app =express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 app.get('/',(req, res)=>{
     res.send('This is the main page');
 })
